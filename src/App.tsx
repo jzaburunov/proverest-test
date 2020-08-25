f142538d79ea60c48209c62c5fd8ec71d8da801d
Juri Zaburunov <juri.zaburunov@gmail.com>
2020-08-25 18:16:15 +0300

    [wip]: Add dummy grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Grid, TableRowInterface } from "./Grid";
 
-export const App = () => {
-  return <div>ololo2</div>;
+// TODO Use Cookie storage to store table
+export const App: React.FC = () => {
+  // 1) Use react-modal
+  // 2) Use react-virtualized for a table
+  // 3) Use cookies for buttons
+  // 4) Use hooks
+  const rowGetter = ({ index }: { index: number }): TableRowInterface => {
+    return {
+      _id: "foo",
+      name: "name",
+      phone: "999",
+      email: "vaisya@gamil.com",
+    };
+  };
+
+  return (
+    <div>
+      {/* TODO Pass handler to open modal cleared */}
+      <div id="header">
+        <button>Add Record</button>
+      </div>
+      {/* TODO Pass here records */}
+      {/* TODO Pass handler to open modal with specific data */}
+      <Grid rowGetter={rowGetter} length={2} />
+      <div id="modal" />
+    </div>
+  );
 };
---
+++ src/Grid.tsx
@@ -0,0 +1,82 @@
+import React, { CSSProperties } from "react";
+import { AutoSizer, Table, Column, TableCellProps } from "react-virtualized";
+
+export const planeFormatter: React.FC<TableCellProps> = ({ cellData }) => (
+  <span>{cellData}</span>
+);
+
+export interface TableRowInterface {
+  _id: string;
+  phone: string;
+  email: string;
+  name: string;
+}
+
+interface GridInterface extends React.HTMLAttributes<HTMLDivElement> {
+  rowGetter({ index }: { index: number }): TableRowInterface;
+  length: number;
+}
+
+export const Grid: React.FC<GridInterface> = (props) => {
+  const { rowGetter, length } = props;
+
+  return (
+    <AutoSizer disableHeight>
+      {({ width }) => (
+        <Table
+          headerHeight={50}
+          height={1000}
+          autoHeight
+          rowGetter={rowGetter}
+          rowHeight={100}
+          rowCount={length}
+          width={width}
+          headerStyle={{
+            fontSize: 12,
+            fontWeight: 600,
+            textTransform: "none",
+          }}
+          rowStyle={({ index }): CSSProperties => {
+            let styles: CSSProperties = {
+              fontWeight: "normal",
+              borderBottom: "1px solid #e2ebf5",
+            };
+            return styles;
+          }}
+        >
+          <Column
+            label="Name"
+            dataKey="name"
+            cellRenderer={planeFormatter}
+            width={240}
+            flexGrow={1}
+          />
+          <Column
+            label="Email"
+            dataKey="email"
+            cellRenderer={planeFormatter}
+            width={210}
+          />
+          <Column
+            label="Phone"
+            dataKey="phone"
+            cellRenderer={planeFormatter}
+            width={240}
+          />
+          <Column
+            label=""
+            dataKey="delete"
+            cellRenderer={({ rowData }) => (
+              <>
+                <button>Edit</button>
+                <button>Remove</button>
+              </>
+            )}
+            width={300}
+            disableSort
+          />
+        </Table>
+      )}
+    </AutoSizer>
+  );
+};
